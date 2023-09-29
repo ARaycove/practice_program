2785d8ffa60b60cdaaf6df42a2e33ee4484801b9
Aaron Raycove <aacra0820@gmail.com>
2023-09-29 12:23:59 -0600

    moved try/except out of definition, pay module now works

--- module/pay_calculator.py
+++ module/pay_calculator.py
@@ -1,9 +1,11 @@
 hours = input ('Please enter your hours for last pay period \n')
 hourly_rate = input('Please enter your hourly rate \n')
-def calculate_overtime(hours, hourly_rate): 
-    try:
-        hours = float(hours)
-        hourly_rate = float(hourly_rate)
+try:
+    hours = float(hours)
+    hourly_rate = float(hourly_rate)
+except: 
+    print('Include only integers')
+def calculate_overtime(hours): 
         if hours > 40:
             overtime_hours = 1.5 * (hours - 40)
             hours = 40
@@ -12,9 +14,5 @@ def calculate_overtime(hours, hourly_rate):
         else:
             total_hours = hours
             return total_hours
-    except: 
-        print('Include only integers')
-        
-
-pay = calculate_overtime(hours, hourly_rate) * hourly_rate #For brevity, we try the hourly_rate conditional inside the function iteself.
+pay = calculate_overtime(hours) * hourly_rate
 print(pay)
