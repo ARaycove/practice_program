c65cd3f905413db8f8828c1d11fe1a0ae61c0985
Aaron Raycove <aacra0820@gmail.com>
2023-09-29 12:16:23 -0600

    updated modules to integrate functions

--- module/grade_prompt.py
+++ module/grade_prompt.py
@@ -1,19 +1,22 @@
 score = input('Enter your grade score, between 0.0 and 1.0 \n')
-try:
-    score = float(score)
-    if score > 1 or score < 0:
-        print("Enter a score between 0 and 1")
-    else:
-        if score >= 0.9:
-            grade = "A"
-        elif score >= 0.8:
-            grade = "B"
-        elif score >= 0.7:
-            grade = "C"
-        elif score >= 0.6:
-            grade = "D"
+def calculate_grade(score):
+    try:
+        score = float(score)
+        if score > 1 or score < 0:
+            print("Enter a score between 0 and 1")
         else:
-            grade = "F"
-        print(grade)
-except:
-    print("include only alpha-numeric characters")
+            if score >= 0.9:
+                grade = "A"
+            elif score >= 0.8:
+                grade = "B"
+            elif score >= 0.7:
+                grade = "C"
+            elif score >= 0.6:
+                grade = "D"
+            else:
+                grade = "F"
+            print(grade)    
+            
+    except:
+        print("include only alpha-numeric characters")
+calculate_grade(score)
--- module/pay_calculator.py
+++ module/pay_calculator.py
@@ -1,15 +1,20 @@
 hours = input ('Please enter your hours for last pay period \n')
 hourly_rate = input('Please enter your hourly rate \n')
-try:
-    hours = float(hours)
-    hourly_rate = float(hourly_rate)
-    if hours > 40:
-        overtime_hours = 1.5 * (hours - 40)
-        hours = 40
-        total_hours = overtime_hours + hours
-        print(total_hours * hourly_rate)
-    else:
-        print(hours * hourly_rate)
-except: 
-    print('Include only alphanumeric characters') 
-    
+def calculate_overtime(hours, hourly_rate): 
+    try:
+        hours = float(hours)
+        hourly_rate = float(hourly_rate)
+        if hours > 40:
+            overtime_hours = 1.5 * (hours - 40)
+            hours = 40
+            total_hours = overtime_hours + hours
+            return total_hours
+        else:
+            total_hours = hours
+            return total_hours
+    except: 
+        print('Include only integers')
+        
+
+pay = calculate_overtime(hours, hourly_rate) * hourly_rate #For brevity, we try the hourly_rate conditional inside the function iteself.
+print(pay)
